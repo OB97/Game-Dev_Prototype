1b072ad09b4207ef5c04daebe797c26a7ce6bbbf
Alex <104856492+OB97@users.noreply.github.com>
2026-06-30 09:56:09 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.core.engine import Engine
 
 def main():
     # Instantiate and start our core game engine loop
-    game = Engine(width=1024, height=768, title="Wizard")
+    game = Engine(width=1024, height=768, title="Prototype")
     game.run()
 
 
